270ba1e552fa162fa297159ba6506f41115e2b68
Chetan Jangir <chetanrjj2002@gmail.com>
2025-10-08 21:03:42 +0530

    made flake more structured

--- flake.nix
+++ flake.nix
@@ -2,19 +2,14 @@
   description = "Chetan's NixOS configuration with Zen Browser + Home Manager";
 
   inputs = {
-    # Nixpkgs source
-
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Home Manager (follows nixpkgs for compatibility)
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
-    # Zen Browser flake
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
@@ -25,32 +20,29 @@
     , ... }@inputs:
     let system = "x86_64-linux";
     in {
-      # 🖥️ Define your NixOS host here
-      nixosConfigurations = {
-        chetan = nixpkgs-stable.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit inputs system; };
 
-          modules = [
+      nixosConfigurations.laptop = nixpkgs-stable.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs system; };
 
-            { nix.settings.experimental-features = [ "nix-command" "flakes" ]; }
-            ./configuration.nix
+        modules = [
 
-            # 🏠 Home Manager module integration
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "bak";
+          { nix.settings.experimental-features = [ "nix-command" "flakes" ]; }
+          ./configuration.nix
 
-                extraSpecialArgs = { inherit inputs; };
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "bak";
 
-                users.chetan = import ./home.nix;
-              };
-            }
-          ];
-        };
+              extraSpecialArgs = { inherit inputs; };
+
+              users.chetan = import ./home.nix;
+            };
+          }
+        ];
       };
     };
 }
