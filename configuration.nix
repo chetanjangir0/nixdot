b7ec57c0b1220b523d34be8a02ca12b3072e5331
Chetan Jangir <chetanrjj2002@gmail.com>
2025-10-09 21:15:35 +0530

    removed some unnecessory comments

--- configuration.nix
+++ configuration.nix
@@ -3,12 +3,6 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
-  #   })
-  # ];
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -16,10 +10,6 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -50,9 +40,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
   services.displayManager.ly.enable = true;
   services.desktopManager.cosmic.enable = true;
 
@@ -62,14 +49,12 @@
   environment.shells = with pkgs; [ zsh ];
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  # because the declarative omz was not respecting my dotfiles
   environment.shellInit = ''
     if [ ! -d "$HOME/.oh-my-zsh" ]; then
        git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
     fi'';
-  # programs.zsh = {
-  #    enable = true;
-  #    ohMyZsh.enable = true;
-  # };
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -77,7 +62,6 @@
     variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = false;
 
   # Enable sound with pipewire.
@@ -88,12 +72,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   fonts.fontconfig.enable = true;
@@ -179,31 +157,9 @@
 
   programs.ssh.startAgent = false; # cosmic de already provides its own
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { 
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
 }
