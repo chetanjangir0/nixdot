36fcba67b4e69acd4670f5c1ac952cdb741c91db
Chetan Jangir <chetanrjj2002@gmail.com>
2025-12-22 18:31:05 +0530

    123 2025-12-22 18:18:41 : enable nix_ld

--- minimal.nix
+++ minimal.nix
@@ -205,4 +205,9 @@ in
     dates = [ "weekly" ];
   };
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged
+    # programs here, NOT in environment.systemPackages
+  ];
 }
