f65a359d5f0b65d2c4139f5677efdfa168180ee4
Chetan Jangir <chetanrjj2002@gmail.com>
2025-12-31 07:16:55 +0530

    125 2025-12-31 07:16:32 : flatpak: Add audiorelay package

--- systemModules/programs/flatpak.nix
+++ systemModules/programs/flatpak.nix
@@ -2,7 +2,7 @@
 
   services.flatpak.enable = true;
 
-  services.flatpak.packages = [ "eu.betterbird.Betterbird" ];
+  services.flatpak.packages = [ "eu.betterbird.Betterbird" "net.audiorelay.AudioRelay"];
 
   # Optional: specify remotes
   services.flatpak.remotes = [{
